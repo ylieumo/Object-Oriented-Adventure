3b0f3071ff4d56d066511b6f4dc49cf64b3a8222
ylieumo <yoldjampou@gmail.com>
2024-01-04 15:12:30 -0500

    First draft

---
+++ ObjectOrientedAdventure.js
@@ -0,0 +1,20 @@
+// Part 1: Humble Beginnings.
+
+
+// Part 2: Class Fantasy.
+
+// 
+// Part 3: Class Features.
+
+
+// Part 4: Class Uniforms.
+
+
+// Part 5: Gather your Party.
+
+
+// Part 6: Developing Skills.
+
+
+// Part 7: Adventure Forth.
+
