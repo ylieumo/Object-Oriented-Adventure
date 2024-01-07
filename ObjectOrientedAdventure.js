c53f71cf09850d53ec37a2fd42d1c62c3a5fe55e
ylieumo <yoldjampou@gmail.com>
2024-01-07 15:55:20 -0500

    Part 3: Class Features.

--- ObjectOrientedAdventure.js
+++ ObjectOrientedAdventure.js
@@ -1,25 +1,25 @@
-// // Part 1: Humble Beginnings.
-// const adventurer = {
-//     name: "Robin",
-//     health: 10,
-//     inventory: ["sword", "potion", "artifact"]
-//     }
+// Part 1: Humble Beginnings.
+const adventurer = {
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "potion", "artifact"]
+    }
 
-//     // From the adventurer object, let access Robin’s inventory using a combination of dot notation and square bracket syntax.
+    // From the adventurer object, let access Robin’s inventory using a combination of dot notation and square bracket syntax.
 
-//    console.log(adventurer.inventory[0]);
+   console.log(adventurer.inventory[0]);
 
 
-// //   create a loop that logs each item in Robin’s inventory.
+//   create a loop that logs each item in Robin’s inventory.
 
-// let loop = "";
+let loop = "";
  
-// for (let x in adventurer){
-//     loop += adventurer[x] + " ";
-// };
-// console.log(loop)
+for (let x in adventurer){
+    loop += adventurer[x] + " ";
+};
+console.log(loop)
 
-// Nested arrays and objects: give Robin’s feline friend a friend of his own.
+Nested arrays and objects: give Robin’s feline friend a friend of his own.
 
 const adventurer = {
     name: "Robin",
@@ -72,6 +72,7 @@ class Character {
       this.health = 100;
       this.inventory = [];
     }
+    // Adding the roll method to the Character class.
     roll (mod = 0) {
         const result = Math.floor(Math.random() * 20) + 1 + mod;
         console.log(`${this.name} rolled a ${result}.`)
@@ -101,7 +102,54 @@ robin.companion.companion.inventory = ["small hat", "sunglasses"];
 console.log(robin.companion.companion.inventory);
 
 // Part 3: Class Features.
-
+class Character {
+    constructor (name) {
+      this.name = name;
+      this.health = 100;
+      this.inventory = [];
+    }
+  }
+  
+  class Adventurer extends Character {
+    constructor (name, role) {
+      super(name);
+      // Adventurers have specialized roles.
+      this.role = role;
+      // Every adventurer starts with a bed and 50 gold coins.
+      this.inventory.push("bedroll", "50 gold coins");
+      //  Every adventurer should have water and food.
+      this.Hydration = Hydration;
+      this.Nutrition = Nutrition;
+      // adventurer have to walk some distance.initial disatnce =0 
+      this.distance = 0 
+    }
+    // Adventurers have the ability to scout ahead of them.
+    scout () {
+      console.log(`${this.name} is scouting ahead...`);
+      super.roll();
+    }
+    // Distance walk overtime 
+    increaseDistance (amount){
+      this.distance += amount;
+      console.log(`${this.name} walked ${amount} km.`)
+    } 
+  }
+  
+  
+  // Create a Companion class with specific properties and methods
+  class Companion extends Character {
+    constructor(name, type) {
+      super(name);
+      this.type = type; 
+      this.isFriendly = true;
+    }
+  
+    // Companion-specific method
+    respectParent() {
+      console.log(`${this.name} is respecting his parent.`);
+    }
+  }
+  
 
 // Part 4: Class Uniforms.
 
